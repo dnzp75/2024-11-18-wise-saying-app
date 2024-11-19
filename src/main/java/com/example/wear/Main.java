c34b201219b4f740925f98d8e6fa6de8e70bb8c6
dnzp75 <dnzp74@g.eulji.ac.kr>
2024-11-19 17:02:41 +0900

    feat: 2단계 구현

--- src/main/java/com/example/wear/Main.java
+++ src/main/java/com/example/wear/Main.java
@@ -13,20 +13,23 @@ public class Main {
 class App {
     public void run(){
         System.out.println("== 명언 앱 ==");
-
         Scanner scanner = new Scanner(System.in);
-
 
         while(true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            System.out.println("입력된 명령어 : %s".formatted(cmd));
+            if(cmd.equals("등록")){
+                System.out.print("명언 : ");
+                scanner.nextLine();
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+            }
 
             if(cmd.equals("종료")){
                 break;
             }
-
         }
 
         scanner.close();
