fd90a5c68d3397466efaf3ac66093fafc179a89a
dnzp75 <dnzp74@g.eulji.ac.kr>
2024-11-19 17:44:52 +0900

    feat: 5단계 구현
    -목록(최신순으로 정렬)

--- src/main/java/com/example/wear/Main.java
+++ src/main/java/com/example/wear/Main.java
@@ -1,5 +1,7 @@
 package com.example.wear;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +13,10 @@ public class Main {
 }
 
 class App {
+
     public void run(){
+        List<WiseSaying> sayings = new ArrayList<>();
+
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
 
@@ -23,14 +28,24 @@ class App {
 
             if(cmd.equals("등록")){
                 System.out.print("명언 : ");
-                scanner.nextLine();
+                String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
-                scanner.nextLine();
+                String author = scanner.nextLine();
 
                 number++;
+                sayings.add(new WiseSaying(number, content, author));
 
                 System.out.println(number + "번 명언이 등록되었습니다.");
+            }
+
+            if(cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = sayings.size() - 1; i >= 0; i--) {
+                    WiseSaying saying = sayings.get(i);
+                    System.out.printf("%d / %s / %s\n", saying.getNumber(), saying.getAuthor(), saying.getContent());
+                }
             }
 
             if(cmd.equals("종료")){
@@ -39,5 +54,31 @@ class App {
         }
 
         scanner.close();
+    }
+
+    private static class WiseSaying {
+        private int number;
+        private String content;
+        private String author;
+
+        public WiseSaying(int number, String content, String author) {
+            this.number = number;
+            this.content = content;
+            this.author = author;
+        }
+
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
     }
 }
