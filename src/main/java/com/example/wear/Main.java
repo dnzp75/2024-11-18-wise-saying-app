d360dc8353db47d127952c489ceaa9b3f2e2e390
dnzp75 <dnzp74@g.eulji.ac.kr>
2024-11-19 14:27:39 +0900

    Scanner

--- src/main/java/com/example/wear/Main.java
+++ src/main/java/com/example/wear/Main.java
@@ -1,5 +1,7 @@
 package com.example.wear;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +14,13 @@ class App {
     public void run(){
         System.out.println("== 명언 앱 ==");
 
-        System.out.println("명령) ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+        scanner.close();
     }
 }
