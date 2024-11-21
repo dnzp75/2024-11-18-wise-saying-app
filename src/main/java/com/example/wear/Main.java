cce6c7c0613b8707af2d45de2c557815e60119fc
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-21 16:51:34 +0900

    feat: 6단계 구현
    - 명언삭제 기능 구현

--- src/main/java/com/example/wear/Main.java
+++ src/main/java/com/example/wear/Main.java
@@ -14,32 +14,32 @@ public class Main {
 
 class App {
 
-    public void run(){
+    public void run() {
         List<WiseSaying> sayings = new ArrayList<>();
 
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
 
-        int number=0;
+        int id = 0;
 
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("등록")){
+            if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                number++;
-                sayings.add(new WiseSaying(number, content, author));
+                id++;
+                sayings.add(new WiseSaying(id, content, author));
 
-                System.out.println(number + "번 명언이 등록되었습니다.");
+                System.out.println(id + "번 명언이 등록되었습니다.");
             }
 
-            if(cmd.equals("목록")){
+            if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
                 for (int i = sayings.size() - 1; i >= 0; i--) {
@@ -47,11 +47,26 @@ class App {
                 }
             }
 
-            if(cmd.equals("종료")){
-                break;
+            if (cmd.startsWith("삭제")) {
+                String[] parts = cmd.split("\\?");
+                int idToDelete = Integer.parseInt(parts[1].substring(3));
+
+                for (int i = 0; i < sayings.size(); i++) {
+                    if (sayings.get(i).matchesId(idToDelete)) {
+                        sayings.remove(i);
+                        System.out.println(idToDelete + "번 명언이 삭제되었습니다.");
+                        break;
+                    }
+                }
+
+                if (cmd.equals("종료")) {
+                    break;
+                }
             }
+
+            scanner.close();
         }
 
-        scanner.close();
     }
 }
+
--- src/main/java/com/example/wear/WiseSaying.java
+++ src/main/java/com/example/wear/WiseSaying.java
@@ -2,18 +2,21 @@ package com.example.wear;
 
 public class WiseSaying {
 
-    private final int number;
+    private final int id;
     private final String content;
     private final String author;
 
-    public WiseSaying(int number, String content, String author) {
-        this.number = number;
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
         this.content = content;
         this.author = author;
     }
 
     public String format() {
-        return String.format("%d / %s / %s", number, author, content);
+        return String.format("%d / %s / %s", id, author, content);
     }
 
+    public boolean matchesId(int idToCompare) {
+        return this.id == idToCompare;
+    }
 }
